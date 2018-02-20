ede92f53b534114f9ed59735c17eefd459a71924
Mdaghi <seifeddine.m'daghi@esprit.tn>
2018-02-20 22:11:40 +0100

    Commit 2 : adding user Entity

---
+++ infinity_server-ejb/src/main/java/tn/esprit/infinity_server/persistence/User.java
@@ -0,0 +1,107 @@
+package tn.esprit.infinity_server.persistence;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private int id;
+	// firstname
+	@Column(name = "firstname", nullable = true, length = 50)
+	private String firstname;
+	// lastname
+	@Column(name = "lastname", nullable = true, length = 50)
+	private String lastname;
+	// login
+	@Column(name = "login", nullable = true, length = 50, unique = true)
+	private String login;
+	// password
+	@Column(name = "password", nullable = true, length = 50)
+	private String password;
+	// activate
+	@Column(name = "activate", nullable = true)
+	private boolean activate;
+	// code
+	@Column(name = "code", nullable = true, length = 50)
+	private String code;
+	// phoneNumber
+	@Column(name = "phoneNumber", nullable = true, length = 9)
+	private String phoneNumber;
+	// email
+	@Column(name = "email", nullable = true, length = 50)
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isActivate() {
+		return activate;
+	}
+	public void setActivate(boolean activate) {
+		this.activate = activate;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
