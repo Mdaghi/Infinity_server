8b73b2b3fa002f79afe7b601f371354ec3985857
Mdaghi <seifeddine.m'daghi@esprit.tn>
2018-02-23 16:47:35 +0100

    Commit 13 : - Creation of AddressRemote interface method - Creation of AddressService EJB implements AddressRemote & EntityManager - Simple Crud in AddressService

--- infinity_server-ejb/src/main/java/tn/esprit/infinity_server/interfaces/AddressRemote.java
+++ infinity_server-ejb/src/main/java/tn/esprit/infinity_server/interfaces/AddressRemote.java
@@ -1,8 +1,17 @@
 package tn.esprit.infinity_server.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import tn.esprit.infinity_server.persistence.Address;
 
 @Remote
 public interface AddressRemote {
+	public void CreateAddress(Address a);
+	public void UpdateAddress(Address a);
+	public void RemoveAddress(Address a);
+	Address findAddressById(Integer id);
+	List<Address> findAllAddress();
 
 }
--- infinity_server-ejb/src/main/java/tn/esprit/infinity_server/services/ServiceAddress.java
+++ infinity_server-ejb/src/main/java/tn/esprit/infinity_server/services/ServiceAddress.java
@@ -1,9 +1,18 @@
 package tn.esprit.infinity_server.services;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import tn.esprit.infinity_server.interfaces.AddressLocal;
 import tn.esprit.infinity_server.interfaces.AddressRemote;
+import tn.esprit.infinity_server.persistence.Address;
 
 /**
  * Session Bean implementation class ServiceAddress
@@ -12,11 +21,51 @@ import tn.esprit.infinity_server.interfaces.AddressRemote;
 @LocalBean
 public class ServiceAddress implements AddressRemote, AddressLocal {
 
+	@PersistenceContext(unitName = "infinity_server-ejb")
+	EntityManager em;
     /**
      * Default constructor. 
      */
-    public ServiceAddress() {
-        // TODO Auto-generated constructor stub
-    }
+  
+
+	@Override
+	public void CreateAddress(Address a) {
+		em.persist(a);
+		
+	}
+
+	@Override
+	public void UpdateAddress(Address a) {
+		em.refresh(a);
+		
+	}
+
+	@Override
+	public void RemoveAddress(Address a) {
+		em.remove(a);
+		
+	}
+
+	@Override
+	public Address findAddressById(Integer id) {
+		Address found = null;
+		String jpql = "select a from Address a where a.id=:id";
+		TypedQuery<Address> query = em.createQuery(jpql, Address.class);
+		query.setParameter("id", id);
+		try {
+			found = query.getSingleResult();
+		} catch (Exception ex) {
+			Logger.getLogger(UserService.class.getName()).log(Level.WARNING,
+					"no such login=" + id);
+		}
+		return found;
+	}
+
+	@Override
+	public List<Address> findAllAddress() {
+		return em.createQuery("select a from Address a", Address.class)
+				.getResultList();
+	}
+	
 
 }
