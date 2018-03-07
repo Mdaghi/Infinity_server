df06f59133d9ac2b1f24f4d10bcc3b80334b6e3e
Oussama Ben Ghorbel <dainerxx.21@gmail.com>
2018-03-07 22:26:06 +0100

    Final version Interfaces Real Time Data Finance

--- infinity_server-ejb/src/main/java/tn/esprit/infinity_server/interfaces/NewsArticleRemote.java
+++ infinity_server-ejb/src/main/java/tn/esprit/infinity_server/interfaces/NewsArticleRemote.java
@@ -4,10 +4,13 @@ import java.net.MalformedURLException;
 import java.util.List;
 import javax.ejb.Remote;
 import tn.esprit.infinity_server.persistence.NewsArticle;
+import tn.esprit.infinity_server.persistence.NewsSource;
 import tn.esprit.infinity_server.persistence.User;
 
 @Remote
 public interface NewsArticleRemote {
+	public void addArticle(NewsArticle a);
+	public void deleteArticle(NewsArticle a);
 	public List<NewsArticle> getAllArticles();
 	public List<NewsArticle> getSavedAritcles(User user);
 	public void userSaveArticle(User user, NewsArticle article);
